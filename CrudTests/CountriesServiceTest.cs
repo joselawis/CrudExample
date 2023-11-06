11d26d675ca7b713b17d8e96bfc3d47afcdaa1ca
joseluiscontreras <joseluis.contreras@innocv.com>
2023-11-06 12:00:05 +0100

    Service project

---
+++ CrudTests/CountriesServiceTest.cs
@@ -0,0 +1,8 @@
+using ServiceContracts;
+
+namespace CrudTests;
+
+public class CountriesServiceTest
+{
+    private readonly ICountriesService _countriesService;
+}
--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -7,5 +7,10 @@ namespace ServiceContracts;
 /// </summary>
 public interface ICountriesService
 {
-    CountryResponse AddCountry(CountryAddRequest? request);
+    /// <summary>
+    ///     Adds a country object to the list of countries
+    /// </summary>
+    /// <param name="countryAddRequest">Country object to add</param>
+    /// <returns>Returns the country object after adding it (including newly generated country id)</returns>
+    CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
 }
