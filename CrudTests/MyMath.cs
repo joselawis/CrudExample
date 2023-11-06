6ab40483ea46f5b21bc0d121093a54ea65ef9c96
joseluiscontreras <joseluis.contreras@innocv.com>
2023-11-06 11:33:46 +0100

    first test

---
+++ CrudTests/MyMath.cs
@@ -0,0 +1,9 @@
+namespace CrudTests;
+
+internal class MyMath
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ CrudTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace CrudTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        const int input1 = 10, input2 = 5;
+        const int expected = 15;
+
+        // Act
+        var actual = MyMath.Add(input1, input2);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
