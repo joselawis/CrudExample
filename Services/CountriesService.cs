27143b3e59404809a4f116b55f173fef1941d860
joseluiscontreras <joseluis.contreras@innocv.com>
2023-11-06 12:27:01 +0100

    AddCountry completed

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,12 +1,30 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services;
 
 public class CountriesService : ICountriesService
 {
+    private readonly List<Country> _countries = new();
+
     public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
     {
-        throw new NotImplementedException();
+        // Validation: countryAddRequest parameter can't be null
+        if (countryAddRequest == null) throw new ArgumentNullException(nameof(countryAddRequest));
+
+        // Validation: countryAddRequest.Name parameter can't be null
+        if (countryAddRequest.CountryName == null) throw new ArgumentException(nameof(countryAddRequest.CountryName));
+
+        // Validation: Duplicate CountryNames are not allowed
+        if (_countries.Any(c => c.CountryName == countryAddRequest.CountryName))
+            throw new ArgumentException("Given country name already exists");
+
+        var country = countryAddRequest.ToCountry();
+        country.CountryId = Guid.NewGuid();
+
+        _countries.Add(country);
+
+        return country.ToCountryResponse();
     }
 }
