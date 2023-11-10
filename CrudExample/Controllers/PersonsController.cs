5c3de1d15e549fd766849239966aabfb1e3fea29
joseluiscontreras <joseluis.contreras@innocv.com>
2023-11-10 15:01:27 +0100

    First page

---
+++ CrudExample/Controllers/PersonsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudExample.Controllers;
+
+public class PersonsController : Controller
+{
+    [Route("persons/index")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
